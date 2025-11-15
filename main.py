60225bae100994387608661066145c77dc934498
Ronen Cohen <ronen.fm@gmail.com>
2025-11-15 16:47:13 +0200

    added tainable_layers hyper parameter

--- main.py
+++ main.py
@@ -412,6 +412,8 @@ if __name__ == "__main__":
     MODEL_TYPE = "efficientnet"  # change to "xception" when you want to run that one
     MODEL_TYPE = "xception"
 
+    trainable_layers = 10
+
     if MODEL_TYPE == "efficientnet":
         img_size = 224
         dataset_root = Path("dataset/dataset_224x224")
@@ -470,9 +472,9 @@ if __name__ == "__main__":
 
     # Build the chosen model
     if build_fn == "efficientnet":
-        detector.build_model(trainable_base_layers=20)
+        detector.build_model(trainable_base_layers=trainable_layers)
     else:  # xception
-        detector.build_modelX(trainable_base_layers=20)
+        detector.build_modelX(trainable_base_layers=trainable_layers)
 
     # Show model architecture
     detector.model.summary()
