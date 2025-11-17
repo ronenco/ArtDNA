2e4c3cc0184168c7958f8a4a9d66142673accb91
Ronen Cohen <ronen.fm@gmail.com>
2025-11-17 12:39:11 +0200

    moved the file fetch_data.py to src/data_prep/

--- src/data_prep/fetch_data.py
+++ src/data_prep/fetch_data.py
@@ -4,9 +4,10 @@ Script to fetch raw data from Hugging Face dataset.
 
 from pathlib import Path
 from huggingface_hub import snapshot_download
+from tokenOption import HF_TOKEN
 
 # Download the dataset
-output_dir = Path("./")
+output_dir = Path("./raw_data")
 output_dir.mkdir(parents=False, exist_ok=True)
 
 print("Downloading ArtDNA dataset from Hugging Face...")
@@ -14,7 +15,8 @@ snapshot_download(
     repo_id="alonttal/ArtDNA",
     repo_type="dataset",
     local_dir=str(output_dir),
-    local_dir_use_symlinks=False
+    local_dir_use_symlinks=False,
+    token=HF_TOKEN
 )
 
 print(f"Download complete! Files saved to {output_dir.absolute()}")
